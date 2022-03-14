8789f74ac473f02c19e642ff66d6cdb589eb8439
Тихомир Найденов <tihomirnaydenov02@pmgkk.com>
2022-03-14 10:39:18 +0200

    Add project files.

---
+++ SeaChess/wwwroot/Scripts/app.js
@@ -0,0 +1,45 @@
+﻿function disableButtons() {
+
+    const button1 = document.getElementById('1').addEventListener('click', () => { button1.disable = true; })
+    const button2 = document.getElementById('2').addEventListener('click', () => { button2.disable = true; })
+    const button3 = document.getElementById('3').addEventListener('click', () => { button3.disable = true; })
+    const button4 = document.getElementById('4').addEventListener('click', () => { button4.disable = true; })
+    const button5 = document.getElementById('5').addEventListener('click', () => { button5.disable = true; })
+    const button6 = document.getElementById('6').addEventListener('click', () => { button6.disable = true; })
+    const button7 = document.getElementById('7').addEventListener('click', () => { button7.disable = true; })
+    const button8 = document.getElementById('8').addEventListener('click', () => { button8.disable = true; })
+    const button9 = document.getElementById('9').addEventListener('click', () => { button9.disable = true; })
+
+    //function setclick(input) {
+    //    preventDefault();
+
+    //    if (input === 1) {
+    //        button1.disable = true;
+    //    }
+    //    else if (input === 2) {
+    //        button2.disable = true;
+    //    }
+    //    else if (input === 3) {
+    //        button3.disable = true;
+    //    }
+    //    else if (input === 4) {
+    //        button4.disable = true;
+    //    }
+    //    else if (input === 5) {
+    //        button5.disable = true;
+    //    }
+    //    else if (input === 6) {
+    //        button6.disable = true;
+    //    }
+    //    else if (input === 7) {
+    //        button7.disable = true;
+    //    }
+    //    else if (input === 8) {
+    //        button8.disable = true;
+    //    }
+    //    else if (input === 9) {
+    //        button9.disable = true;
+    //    }
+    //}
+}
+disableButtons();
